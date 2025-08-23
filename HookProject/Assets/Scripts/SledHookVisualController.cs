122e2ecc6f8e81f85ff7e2ce6d9a2bf123146a1a
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-08-22 22:50:22 -0400

    fix: Hook not disabling on unequip

--- HookProject/Assets/Scripts/SledHookVisualController.cs
+++ HookProject/Assets/Scripts/SledHookVisualController.cs
@@ -73,6 +73,6 @@ public class SledHookVisualController : MonoBehaviour
 
     private void DisableSledHook()
     {
-        sledHookRenderer.enabled = true;
+        sledHookRenderer.enabled = false;
     }
 }
